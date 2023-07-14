05c8878def91904918541a463f5206f176844630
cytuswot <umraokhushi73@gmail.com>
2023-07-14 19:57:03 +0530

    better variable names

--- practice/area_of_circle.py
+++ practice/area_of_circle.py
@@ -2,7 +2,7 @@
 WAP in python to calculate the area of circle
 """
 
-r = float(input('Enter radius of circle: '))
-a_c = 3.14*r*r
-print('Area of cirle = ',a_c)
+radius = float(input('Enter radius of circle: '))
+area = 3.14 * radius * radius
+print('Area of cirle = ',area)
  
